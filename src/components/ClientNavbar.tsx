dc5861cb9ceab8d2061422092f5f3bce38c0d599
miguechiape <miguechiape@gmail.com>
2025-09-11 10:55:04 -0300

    solucion 2.0

--- src/components/ClientNavbar.tsx
+++ src/components/ClientNavbar.tsx
@@ -66,14 +66,7 @@ const ClientNavbar = memo(() => {
         {/* Círculo flotante animado */}
         <motion.div
           className="absolute -top-7 w-14 h-14 bg-peach-200 rounded-full flex items-center justify-center shadow-md z-20"
-          initial={{
-            left:
-              activeItem === "rewards"
-                ? "18%"
-                : activeItem === "home"
-                ? "48%"
-                : "82%",
-          }}
+          initial={{ left: "48%" }}
           animate={{
             left:
               activeItem === "rewards"
@@ -103,14 +96,7 @@ const ClientNavbar = memo(() => {
         {/* Línea negra que se desplaza con la barra */}
         <motion.div
           className="absolute top-11 w-16 h-1 bg-black rounded-full z-10"
-          initial={{
-            left:
-              activeItem === "rewards"
-                ? "18%"
-                : activeItem === "home"
-                ? "48%"
-                : "82%",
-          }}
+          initial={{ left: "48%" }}
           animate={{
             left:
               activeItem === "rewards"
@@ -143,15 +129,7 @@ const ClientNavbar = memo(() => {
                 <motion.path
                   d="M110 30C85 30 85.5 70 55 70C24.5 70 25 30 0 30C0 10 35 0 55 0C75 0 110 13 110 30Z"
                   fill="black"
-                  initial={{
-                    x:
-                      activeItem === "rewards"
-                        ? 68.4 - 55
-                        : activeItem === "home"
-                        ? 182.4 - 55
-                        : 311.6 - 55,
-                    y: -30,
-                  }}
+                  initial={{ x: 182.4 - 55, y: -30 }}
                   animate={{
                     x:
                       activeItem === "rewards"
