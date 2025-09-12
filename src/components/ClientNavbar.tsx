3965825549c693e5c7380ccb8f4bb733f18aab88
miguechiape <miguechiape@gmail.com>
2025-09-12 13:47:42 -0300

    6.4

--- src/components/ClientNavbar.tsx
+++ src/components/ClientNavbar.tsx
@@ -190,9 +190,6 @@ const ClientNavbar = memo(() => {
         {/* Círculo flotante animado */}
         <motion.div
           className="navbar-circle absolute -top-7 w-14 h-14 bg-peach-200 rounded-full flex items-center justify-center shadow-md z-20"
-          initial={{
-            x: currentPosition.circle - 218
-          }}
           animate={{
             x: currentPosition.circle - 218  // Centrar respecto al contenedor de 380px
           }}
@@ -212,9 +209,6 @@ const ClientNavbar = memo(() => {
         {/* Línea negra que se desplaza con la barra */}
         <motion.div
           className="navbar-line absolute top-11 w-16 h-1 bg-black rounded-full z-10"
-          initial={{
-            x: currentPosition.circle - 218
-          }}
           animate={{
             x: currentPosition.circle - 218  // Centrar respecto al contenedor de 380px
           }}
@@ -238,10 +232,6 @@ const ClientNavbar = memo(() => {
                 <motion.path
                   d="M110 30C85 30 85.5 70 55 70C24.5 70 25 30 0 30C0 10 35 0 55 0C75 0 110 13 110 30Z"
                   fill="black"
-                  initial={{
-                    x: getSVGPosition(currentPosition.circle),
-                    y: -30
-                  }}
                   animate={{
                     x: getSVGPosition(currentPosition.circle),
                     y: -30
