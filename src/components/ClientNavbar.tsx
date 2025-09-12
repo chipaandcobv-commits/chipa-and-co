3fc2d8e78b34395ed20cc1c495c055d6599edad6
miguechiape <miguechiape@gmail.com>
2025-09-12 13:26:32 -0300

    6.2

--- src/components/ClientNavbar.tsx
+++ src/components/ClientNavbar.tsx
@@ -76,6 +76,15 @@ const ClientNavbar = memo(() => {
     return positions.home;
   }, [isActive, positions]);
 
+  // Configuración de transición compartida para sincronizar todas las animaciones
+  const sharedTransition = {
+    type: "spring" as const,
+    stiffness: 300,
+    damping: 30,
+    duration: 0.6
+  };
+
+
   const navItems = [
     {
       id: "rewards",
@@ -181,50 +190,31 @@ const ClientNavbar = memo(() => {
         {/* Círculo flotante animado */}
         <motion.div
           className="navbar-circle absolute -top-7 w-14 h-14 bg-peach-200 rounded-full flex items-center justify-center shadow-md z-20"
-          initial={{
-            left: `${currentPosition.circle}px`
-          }}
           animate={{
-            left: `${currentPosition.circle}px`
+            x: currentPosition.circle - 218  // Centrar respecto al contenedor de 380px
           }}
-          transition={{
-            type: "spring",
-            stiffness: 300,
-            damping: 30
-          }}
+          transition={sharedTransition}
           style={{
+            left: "50%",
             transform: "translateX(-50%)"
           }}
         >
           <div className="navbar-icon">
-            <motion.div
-              key={activeItem} // Key para forzar la re-renderización y animación de salida/entrada del ícono
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.2 }}
-            >
-              {activeItem === "rewards" && <GiftCardIcon className="w-6 h-6 text-[#F15A25]" />}
-              {activeItem === "home" && <HomeIcon className="w-6 h-6 text-[#F15A25]" />}
-              {activeItem === "profile" && <UserIcon className="w-6 h-6 text-[#F15A25]" />}
-            </motion.div>
+            {activeItem === "rewards" && <GiftCardIcon className="w-6 h-6 text-[#F15A25]" />}
+            {activeItem === "home" && <HomeIcon className="w-6 h-6 text-[#F15A25]" />}
+            {activeItem === "profile" && <UserIcon className="w-6 h-6 text-[#F15A25]" />}
           </div>
         </motion.div>
 
         {/* Línea negra que se desplaza con la barra */}
         <motion.div
           className="navbar-line absolute top-11 w-16 h-1 bg-black rounded-full z-10"
-          initial={{
-            left: `${currentPosition.circle}px`
-          }}
           animate={{
-            left: `${currentPosition.circle}px`
+            x: currentPosition.circle - 218  // Centrar respecto al contenedor de 380px
           }}
-          transition={{
-            type: "spring",
-            stiffness: 300,
-            damping: 30
-          }}
+          transition={sharedTransition}
           style={{
+            left: "50%",
             transform: "translateX(-50%)"
           }}
         />
@@ -237,24 +227,16 @@ const ClientNavbar = memo(() => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <defs>
-              <mask id="bar-mask"> {/* ID fija */}
+              <mask id="bar-mask">
                 <rect width="380" height="50" fill="white" rx="25" />
                 <motion.path
                   d="M110 30C85 30 85.5 70 55 70C24.5 70 25 30 0 30C0 10 35 0 55 0C75 0 110 13 110 30Z"
                   fill="black"
-                  initial={{
-                    x: getSVGPosition(currentPosition.circle),
-                    y: -30
-                  }}
                   animate={{
                     x: getSVGPosition(currentPosition.circle),
                     y: -30
                   }}
-                  transition={{
-                    type: "spring",
-                    stiffness: 300,
-                    damping: 30
-                  }}
+                  transition={sharedTransition}
                 />
               </mask>
             </defs>
